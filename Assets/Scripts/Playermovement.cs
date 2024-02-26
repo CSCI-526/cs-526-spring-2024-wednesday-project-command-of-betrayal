1f3da3248010184fe3797b15a72ba7bb966cddac
ayushyelne <83401078+ayushyelne@users.noreply.github.com>
2024-02-25 18:47:09 -0800

    Updated coin-things

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Playermovement : MonoBehaviour
 {
@@ -17,6 +18,16 @@ public class Playermovement : MonoBehaviour
             Vector2 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * speed * Time.deltaTime);
         }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Reward"))
+        {
+            Destroy(collision.gameObject);
+            //score = score + 1;
+            //scoreText.text = score.ToString();
+        }
+    }
 }
 
 
