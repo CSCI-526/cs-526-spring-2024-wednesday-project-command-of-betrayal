860bc3ec7aeb03ab5354f1b4dd7da79c102a6de7
ishannjr <87802247+ishannjr@users.noreply.github.com>
2024-03-26 16:34:30 -0700

    Bug fix

--- Assets/Scripts/waypoints.cs
+++ Assets/Scripts/waypoints.cs
@@ -1,49 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class waypoints : MonoBehaviour
 {
-    public Transform[] waypointTargets; 
-    public float speed = 2f;        
-    private int waypointIndex = 0;  
+    public Transform[] waypointTargets;
+    public float speed = 2f;
+    private int waypointIndex = 0;
+    private Quaternion originalRotation; 
 
     void Start()
     {
        
         transform.position = waypointTargets[waypointIndex].position;
+       
+        originalRotation = transform.rotation;
     }
 
     void Update()
     {
-      
         MoveTowardsWaypoint();
     }
 
     void MoveTowardsWaypoint()
     {
-        float distance = Vector3.Distance(transform.position, waypointTargets[waypointIndex].position);
+        float step = speed * Time.deltaTime; 
+        transform.position = Vector3.MoveTowards(transform.position, waypointTargets[waypointIndex].position, step);
 
-        if (distance > 0.1f)
+       
+        if (Vector3.Distance(transform.position, waypointTargets[waypointIndex].position) < 0.1f)
         {
-            
-            float step = speed * Time.deltaTime;
+           
+            transform.rotation = originalRotation;
 
-          
-            transform.position = Vector3.MoveTowards(transform.position, waypointTargets[waypointIndex].position, step);
-        }
-        else
-        {
             
             transform.Rotate(0, 180, 0, Space.Self);
 
            
             waypointIndex++;
 
-           
+            
             if (waypointIndex >= waypointTargets.Length)
             {
                 waypointIndex = 0;
             }
+
+         
+            originalRotation = transform.rotation;
         }
     }
 }
