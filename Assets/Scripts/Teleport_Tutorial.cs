0dfdf2481cf5e30be18e5fc6b8ac0c1772bfe691
ayushyelne <83401078+ayushyelne@users.noreply.github.com>
2024-04-18 18:57:46 -0700

    minor changes

--- Assets/Scripts/Teleport_Tutorial.cs
+++ Assets/Scripts/Teleport_Tutorial.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teleport : MonoBehaviour
+public class Teleport_Tutorial : MonoBehaviour
 {
    private Transform destination;
 
