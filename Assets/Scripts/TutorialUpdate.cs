378c0e507679b78c0cbaec6f9c5798429482f9b4
Mudit Mohan Aggarwal <Mudit.m.aggarwal@gmail.com>
2024-04-24 23:28:31 +0530

    Tutorial updated

--- Assets/Scripts/TutorialUpdate.cs
+++ Assets/Scripts/TutorialUpdate.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class TutorialUpdate : MonoBehaviour
 {
-    //public Text tutorialText;
     public GameObject player;
+    public GameObject collectibleItem;
+    public Animator textAnimator;
 
     private int step = 0;
+    private bool itemCollected = false;
+
+    void Start()
+    {
+
+        //tutorialText.text = "Welcome to the tutorial! Let's learn the basics.";
+        if (textAnimator != null)
+        {
+            textAnimator.SetTrigger("Highlight");
+        }
+    }
 
     void Update()
     {
@@ -17,20 +29,45 @@ public class TutorialUpdate : MonoBehaviour
                 //tutorialText.text = "Use the arrow keys to move left and right.";
                 if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
                 {
-                    step++;
+                    NextStep();
                 }
                 break;
             case 1:
                 //tutorialText.text = "Press 'Space' to jump.";
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    step++;
+                    NextStep();
                 }
                 break;
             case 2:
+                //tutorialText.text = "Now, collect the item by touching it.";
+                if (itemCollected)
+                {
+                    NextStep();
+                }
+                break;
+            case 3:
                 //tutorialText.text = "Great! Now reach the end of the level to complete the tutorial.";
+
                 break;
         }
     }
-}
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == collectibleItem)
+        {
+            itemCollected = true;
+            Destroy(collectibleItem);
+        }
+    }
+
+    private void NextStep()
+    {
+        step++;
+        if (textAnimator != null)
+        {
+            textAnimator.SetTrigger("Highlight");
+        }
+    }
+}
